58b2d7557f54e824a8d8f6805f386ed75440fde1
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-05-08 11:08:54 -0600

    feat (minor) Change 'submit' to button

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
+console.log("app.js is hooked up!");
+
 const balance = (function() {
   var input = [];
   var inputString = "";
 
   const incomeTotal = function(a, b, c, d, e) {
-    let inSum = a+b+c+d+e;
-    return inSum;
+    let incSum = a+b+c+d+e;
+    return incSum;
   };
 
   const expensesTotal = function(a, b, c, d, e) {
@@ -17,3 +19,29 @@ const balance = (function() {
     return bal;
   }
 })
+
+balance();
+// --------------------------
+// Or maybe this is the way to do it?
+
+
+function addListenerForIncomeSum() {
+  const sumButton =document.querySelector("#totalIncome");
+  button.addEventListener("click", function() {
+    incomeTotal();
+  })
+}
+
+function addListenerForExpensesSum() {
+  const sumButton =document.querySelector("#totalExpenses");
+  button.addEventListener("click", function() {
+    expensesTotal();
+  })
+}
+
+function addListenerForBalance() {
+  const sumButton =document.querySelector("#balance");
+  button.addEventListener("click", function() {
+    expensesTotal();
+  })
+}
